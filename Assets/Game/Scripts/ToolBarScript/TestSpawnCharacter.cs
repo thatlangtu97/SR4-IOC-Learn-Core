b13ad641d9ee669e30b9c9d694ecdfae41231645
ToanCute <toanlt@bravestars.com>
2022-04-13 18:44:51 +0700

    push

--- Assets/Game/Scripts/ToolBarScript/TestSpawnCharacter.cs
+++ Assets/Game/Scripts/ToolBarScript/TestSpawnCharacter.cs
@@ -7,32 +7,47 @@ using Random = UnityEngine.Random;
 public class TestSpawnCharacter : MonoBehaviour
 {
     public bool SetPlayer;
+    public float TimeDelay;
     public void Spawn(GameObject prefab)
     {
         StateMachineController temp = null;
         int index = 0;
         while (index<count)
         {
-            //temp = ObjectPool.Spawn(prefab).GetComponent<StateMachineController>();
+//            //temp = ObjectPool.Spawn(prefab).GetComponent<StateMachineController>();
+//            temp = PoolManager.Spawn(prefab.GetComponent<PoolItem>()).GetComponent<StateMachineController>();
+//            //temp.GetComponent<ComponentManager>().SetupEntity();
+//            
+//            
+//            //ActionBufferManager.Instance.ActionDelayTime(() => { temp.GetComponent<ComponentManager>().SetupEntity();}, 2f);
+//            
+//            //temp.gameObject.SetActive(true);
+//            Action tempAction = delegate {  temp.GetComponent<ComponentManager>().SetupEntity(); };
+//            setActionDelay(tempAction, TimeDelay);
+            
+            
             temp = PoolManager.Spawn(prefab.GetComponent<PoolItem>()).GetComponent<StateMachineController>();
-            //temp.GetComponent<ComponentManager>().SetupEntity();
+            Action tempAction = delegate {  temp.GetComponent<ComponentManager>().SetupEntity(); };
+            setActionDelay(tempAction, TimeDelay);
             
-            
-            ActionBufferManager.Instance.ActionDelayTime(() => { temp.GetComponent<ComponentManager>().SetupEntity();}, 2f);
-            
-            //temp.gameObject.SetActive(true);
             index += 1;
         } 
         
         
         if (SetPlayer)
         {
-            ActionBufferManager.Instance.ActionDelayTime(() =>
-            {
+            Action tempAction = delegate {  
                 GameUIController.instance.stateMachine = temp;
-                GameUIController.instance.MODIFY();
-                
-            }, 2.1f);
+                GameUIController.instance.MODIFY(); 
+            };
+            
+            setActionDelay(tempAction, TimeDelay);
+//            ActionBufferManager.Instance.ActionDelayTime(() =>
+//            {
+//                GameUIController.instance.stateMachine = temp;
+//                GameUIController.instance.MODIFY();
+//                
+//            }, 2.1f);
             
         }
     }
@@ -61,13 +76,17 @@ public class TestSpawnCharacter : MonoBehaviour
         float timeDelay = 1f;
         while (countSpawn< count )
         {
-            Vector3 randomPos = new Vector3(Random.Range(-13f,5f),0f,0f);
-            Action action = delegate 
-            {
-                StateMachineController temp = ObjectPool.Spawn(prefab,null,randomPos).GetComponent<StateMachineController>();
-                temp.gameObject.SetActive(true);  
-            };
-            ActionBufferManager.Instance.ActionDelayTime(action,timeDelay*countSpawn);
+//            Vector3 randomPos = new Vector3(Random.Range(-13f,5f),0f,0f);
+//            Action action = delegate 
+//            {
+//                StateMachineController temp = ObjectPool.Spawn(prefab,null,randomPos).GetComponent<StateMachineController>();
+//                temp.gameObject.SetActive(true);  
+//            };
+//            ActionBufferManager.Instance.ActionDelayTime(action,timeDelay*countSpawn);
+//            
+            StateMachineController temp = PoolManager.Spawn(prefab.GetComponent<PoolItem>()).GetComponent<StateMachineController>();
+            Action tempAction = delegate {  temp.GetComponent<ComponentManager>().SetupEntity(); };
+            setActionDelay(tempAction, timeDelay);
             //timeDelay *= countSpawn;
             countSpawn += 1;
         }
@@ -89,6 +108,69 @@ public class TestSpawnCharacter : MonoBehaviour
         {
             Application.targetFrameRate = -1;
             fps = -1;
+        }
+    }
+
+    private void Update()
+    {
+        if (actions.Count > 0)
+        {
+            foreach (var action in actions)
+            {
+                if (action.CanInvoke)
+                {
+                    action.Invoke();
+                }
+                else
+                {
+                    action.DownTime(Time.deltaTime);
+                }
+            }
+
+            foreach (var action in actions)
+            {
+                if (action.CanRemove)
+                {
+                    actions.Remove(action);
+                    break;
+                }
+            }
+        }
+    }
+
+    public List<DataAction> actions= new List<DataAction>();
+    public void setActionDelay(Action actionNew, float time)
+    {
+        if (actions == null)
+        {
+            actions= new List<DataAction>();
+        }
+        actions.Add(new DataAction(){action = actionNew,timeDelay = time,isInvoke = false});
+    }
+    [System.Serializable]
+    public class DataAction
+    {
+        public Action action;
+        public float timeDelay;
+        public bool isInvoke;
+
+        public bool CanInvoke
+        {
+            get { return isInvoke == false && timeDelay < 0; }
+        }
+        public bool CanRemove
+        {
+            get { return isInvoke == true && timeDelay < 0; }
+        }
+        public void DownTime(float Time)
+        {
+            timeDelay -= Time;
+        }
+
+        public void Invoke()
+        {
+            action.Invoke();
+            isInvoke = true;
         }
     }
 }
