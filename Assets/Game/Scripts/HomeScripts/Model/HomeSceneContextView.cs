098921e5184add047cce5e67520aaf05f224f4b5
Toan <toantrongluong@gmail.com>
2022-04-18 00:37:44 +0700

    fix Home Scene init Entry null

--- Assets/Game/Scripts/HomeScripts/Model/HomeSceneContextView.cs
+++ Assets/Game/Scripts/HomeScripts/Model/HomeSceneContextView.cs
@@ -1,10 +1,29 @@
-﻿using strange.extensions.context.impl;
+﻿using System;
+using strange.extensions.context.impl;
 using System.Collections;
 using System.Collections.Generic;
+using EntrySystem;
 using UnityEngine;
 
 public class HomeSceneContextView : ContextView
 {
+    private void Awake()
+    {
+        if (EntryContextView.Instance != null)
+        {
+            //EntryContextView.Instance.loadFlashScene = false;
+        }
+        
+        InitUI();
+    }
+    public void InitUI()
+    {
+        GameObject UI1 = Resources.Load<GameObject>(GameResourcePath.UI1);
+        GameObject UI2 = Resources.Load<GameObject>(GameResourcePath.UI2);
+        Instantiate(UI1);
+        Instantiate(UI2);
+    }
+
     void Start()
     {
         context = new HomeSceneContext(this);
