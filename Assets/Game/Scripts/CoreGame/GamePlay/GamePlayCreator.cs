5b8b012d40de7b0c1333b61c1837aa8f78c01a6f
ToanCute <toanlt@bravestars.com>
2022-04-19 19:05:52 +0700

    push gameplay creator - step 1 create Hero

--- Assets/Game/Scripts/CoreGame/GamePlay/GamePlayCreator.cs
+++ Assets/Game/Scripts/CoreGame/GamePlay/GamePlayCreator.cs
@@ -1,8 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class GamePlayCreator : MonoBehaviour
 {
+    static GamePlayCreator instance;
+    public string heroPrefabName;
+    private PoolItem heroPrefab;
+    public static GamePlayCreator Instance {
+        get { return instance; }
+    }
     
+    private void Awake()
+    {
+        heroPrefab = Resources.Load<PoolItem>("PrefabCharacter/" + heroPrefabName);
+    }
+    [Button("SPAWN HERO", ButtonSizes.Gigantic), GUIColor(0.4f, 0.8f, 1),]
+    public void CreateHero()
+    {
+        
+        StateMachineController statemachine = PoolManager.Spawn(heroPrefab).GetComponent<StateMachineController>();
+        statemachine.GetComponent<ComponentManager>().SetupEntity();
+        GameUIController.instance.stateMachine = statemachine;
+        GameUIController.instance.MODIFY(); 
+    }
 }
